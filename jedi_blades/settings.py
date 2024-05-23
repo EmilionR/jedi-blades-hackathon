cc6511225496cd6b86953b4bdd6e8b06a90a9897
salms358 <sa083850@gmail.com>
2024-05-23 12:49:42 +0000

    updating models

--- jedi_blades/settings.py
+++ jedi_blades/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-emilionr-jedibladeshack-k4fo3uqmrrx.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
