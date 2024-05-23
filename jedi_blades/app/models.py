0f9b4ba1acb9c3208239286fae80980be99fd264
salms358 <sa083850@gmail.com>
2024-05-23 16:49:09 +0000

    changing the owner to foreign key adding the home planet field

--- jedi_blades/app/models.py
+++ jedi_blades/app/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-class Lightsaber(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    color = models.CharField(max_length=20)
-    owner = models.CharField(max_length=100)
-    design = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.name} ({self.color})"
-
 
 class Character(models.Model):
     ALIGNMENT_CHOICES = [
@@ -18,11 +8,22 @@ class Character(models.Model):
     ]
 
     name = models.CharField(max_length=100, unique=True)
+    image = models.URLField(null=True, blank=True)
     full_name = models.CharField(max_length=200)
     alignment = models.CharField(max_length=1, choices=ALIGNMENT_CHOICES)
+    home_planet = models.ForeignKey(max_length=50)
     lightsaber = models.ForeignKey(Lightsaber, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
+class Lightsaber(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    color = models.CharField(max_length=20)
+    owner = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='lightsabers')
+    design = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.color})"
