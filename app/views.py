e725d5e7cc08b10eee0e5cef80e3bb6a32fa5362
Emilion <emiljadersten@gmail.com>
2024-05-23 15:38:15 +0200

    Create a list view for the lightsaber model

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Lightsaber, Character
 
 # Create your views here.
+
+class LightsaberListView(ListView):
+    model = Lightsaber
+    template_name = 'lightsaber_list.html'  # This is the template we'll use to render the lightsabers page
+    context_object_name = 'lightsabers'  # The context object we'll use in the template
