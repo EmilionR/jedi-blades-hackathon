76a3587c29b8a719d3c939893089a8d966496723
Jesse Lee Ross <jesseross001@gmail.com>
2024-05-25 15:41:14 +0000

    update charector model with lore

--- app/models.py
+++ app/models.py
@@ -23,6 +23,6 @@ class Character(models.Model):
     alignment = models.CharField(max_length=1, choices=ALIGNMENT_CHOICES)
     home_planet = models.CharField(max_length=100)
     profile_image = CloudinaryField('image', default='https://res.cloudinary.com/digqsxx3z/image/upload/v1716572753/DALL_E_2024-05-22_21.57.28_-_An_imposing_landscape_of_a_Sith_stronghold_on_a_barren_volcanic_planet_where_lava_rivers_flow_freely_under_a_stormy_ash-filled_sky._The_stronghold_i_ilacid.webp')
-
+    lore = models.TextField(default='Details about the character\'s backstory, significant events, and role in the Star Wars universe.')
     def __str__(self):
         return self.name
